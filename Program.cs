4e5f9f6db2733fafa41e3360769a3d12516506a0
joaovitorfuhr <joao.fuhr@institutoivoti.org.br>
2017-10-17 20:58:03 -0200

    aumento do código

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace hwapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nome;
+            nome="Institutoivoti";
+            // Console.WriteLine("Hello World!");
+            Console.WriteLine("Institutoivoti");
+
+            Double valor = 2.2;
+            Double quantidade = 6.567;
+            Double resultado = valor * quantidade;
+            System.Console.WriteLine(resultado);
+
+            Boolean maiorQueDez = valor > 10;
+            if(maiorQueDez) {
+                
+            }
         }
     }
 }
