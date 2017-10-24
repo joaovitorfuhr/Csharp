48afe6486011adb00aa3b65d9f1cb5947a9e78a8
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-10-24 21:14:15 -0200

    Add files via upload
    criacao de números aleatorios

---
+++ Numeros.cs
@@ -0,0 +1,14 @@
+using System;
+
+Namespace console-dotnet
+{
+    class Numeros
+    {
+        string numero = string.Empty;
+        Random rndNumero = new Random();
+        numero = rndNumero.Next().ToString();
+
+    }
+
+
+}
