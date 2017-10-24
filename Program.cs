6b41eb0cde3af8d61a45b99199d74118d33e8e4f
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-10-24 20:04:35 -0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,30 @@
 ﻿using System;
 
-namespace hwapp
+namespace console_dotnet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string nome;
-            nome="Institutoivoti";
-            // Console.WriteLine("Hello World!");
-            Console.WriteLine("Institutoivoti");
+            String nome;
+            nome = "eu";
+            Console.WriteLine(nome);
 
             Double valor = 2.2;
-            Double quantidade = 6.567;
+            Double quantidade = 6;
             Double resultado = valor * quantidade;
             System.Console.WriteLine(resultado);
 
-            Boolean maiorQueDez = valor > 10;
-            if(maiorQueDez) {
-               System.Console.WriteLine($"{resultado} é maior que 10");
-               }else{
-                   System.Console.WriteLine($"{resultado} é menor que 10");
-               } 
+            Boolean maiorQueDez = (resultado > 10);
+
+            if (maiorQueDez) {
+                System.Console.WriteLine($"{resultado} é maior que 10");
+            } else {
+                System.Console.WriteLine($"{resultado} é menor que 10");
+            }
+
+            calcsoma calcsoma = new calcsoma();
+            System.Console.WriteLine(calcsoma.soma(10, 5));
         }
     }
 }
---
+++ calcsoma.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace console_dotnet
+{
+    class calcsoma
+    {
+        public int soma(int a, int b) {
+                return a + b;
+        }
+    }
+}
