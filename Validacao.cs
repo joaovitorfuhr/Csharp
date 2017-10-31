69e85a96b8dc362db4936b503966eeabf4f76fd9
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-10-31 19:26:29 -0200

    Validacao

---
+++ Validacao.cs
@@ -0,0 +1,23 @@
+
+using System;
+
+namespace console_dotnet
+{    
+    class Validacao
+    {
+        public void numero(int a, int b) {
+            Random r = new Random();
+            int n = r.Next(a, b);
+            int s = r.Next(a, b);
+            int resul = n + s;
+            System.Console.WriteLine(n + " + " + s + " = ");
+            int resposta = Convert.ToInt32(Console.ReadLine());
+
+            if (resposta == resul) {
+                System.Console.WriteLine("Acesso liberado!");
+            } else {
+                System.Console.WriteLine("Acesso bloqueado!");
+            }
+        }
+    }
+}
